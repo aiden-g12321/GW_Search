4b126f46be1e3c3037c8d0d2ace329bde1bebf47
Aiden Gundersen <aidengundersen36@gmail.com>
2024-04-19 11:44:44 -0600

    "added get_data method"

---
+++ get_data.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from gwpy.timeseries import TimeSeries
+
+
+def get_data(start_time, end_time, plot_data=False):
+    
+    # fetch data and times
+    time_seriesH1 = TimeSeries.fetch_open_data('H1', start_time, end_time, sample_rate=4096)
+    time_seriesL1 = TimeSeries.fetch_open_data('L1', start_time, end_time, sample_rate=4096)
+    data_H1 = np.array(time_seriesH1)
+    data_L1 = np.array(time_seriesL1)
+    times = np.array(time_seriesH1.times)
+
+    # save data to .txt file
+    np.savetxt('times.txt', times)
+    np.savetxt('data_H1.txt', data_H1)
+    np.savetxt('data_L1.txt', data_L1)
+
+    # plot data
+    if plot_data:
+        plt.plot(times, data_H1, label='Hanford')
+        plt.plot(times, data_L1, label='Livingston')
+        plt.legend(loc='upper right')
+        plt.show()
+        
+    return
+
+
+t0 = 1126259446
+t1 = 1126259478
+get_data(t0, t1, plot_data=True)
+        
+
