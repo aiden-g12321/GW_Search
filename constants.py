848bd23a0120dd2c7600cc367ef377d49fb9effa
Aiden Gundersen <aidengundersen36@gmail.com>
2024-04-20 15:22:55 -0600

    "constant labels"

--- constants.py
+++ constants.py
@@ -6,18 +6,18 @@ eventname = 'GW200311_115853'
 GPS_start = 1267963091
 
 # unit conversions
-MTSUN_SI = 4.925491025543575903411922162094833998e-6  # Geometrized solar mass, s
-MSUN_SI = 1.988546954961461467461011951140572744e30  # Solar mass, kg
-MRSUN_SI = 1.476625061404649406193430731479084713e3  # Geometrized solar mass, m
-CLIGHT = 2.99792458e8  # Speed of light, m/s
-PC_SI = 3.085677581491367278913937957796471611e16  # parsec, m
+MTSUN_SI = 4.925491025543575903411922162094833998e-6  # 1 geometrized Solar mass in seconds
+MSUN_SI = 1.988546954961461467461011951140572744e30  # 1 Solar mass in kilograms
+MRSUN_SI = 1.476625061404649406193430731479084713e3  # 1 geometrized Solar mass in meters
+CLIGHT = 2.99792458e8  # speed of light, m/s
+PC_SI = 3.085677581491367278913937957796471611e16  # 1 parsec in meters
 
 
 # measured parameters
 m1_measured = 34.2  # solar masses
 m2_measured = 27.7  # solar masses
-m1_measured_sec = m1_measured * MTSUN_SI
-m2_measured_sec = m2_measured * MTSUN_SI
+m1_measured_sec = m1_measured * MTSUN_SI  # seconds
+m2_measured_sec = m2_measured * MTSUN_SI  # seconds
 
 num_params = 5
 
