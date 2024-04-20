a4e9d43af92b8726963fb4e6554895bee5cb3bb1
Aiden Gundersen <aidengundersen36@gmail.com>
2024-04-19 21:30:43 -0600

    "added waveform generation"

--- constants.py
+++ constants.py
@@ -1,10 +1,9 @@
 eventname = 'GW200311_115853'
 GPS_start = 1267963091
 
-# define measured component masses, luminosity distance, time and phase
+
+# parameters
+# luminosity distance, time and phase
 m1_measured = 34.2  # solar masses
 m2_measured = 27.7  # solar masses
 ms_measured = [m1_measured, m2_measured]
-DL = 100.  # Mega-parsecs
-t0 = 0.
-phi0 = 0.
--- get_waveform_A.py
+++ get_waveform_A.py
@@ -5,8 +5,8 @@ from constants import *
 
 
 # function to change mass coordinates
-def mass_transform(component_masses):
-    m1, m2 = component_masses
+def mass_transform(comp_masses):
+    m1, m2 = comp_masses
     M = m1 + m2
     eta = m1 * m2 / M**2
     return np.array([M, eta])
@@ -14,24 +14,27 @@ def mass_transform(component_masses):
 
 # modify PhenomA methods so they take component masses as input
 # get amplitude of signal in frequency-domain
-def get_amp(fs, comp_masses):
-    M, eta = mass_transform(comp_masses)
-    return Aeff(fs, M, eta, Dl=DL)
+def get_amp(fs, params):
+    m1, m2, t0, phi0, Dl = params
+    M, eta = mass_transform([m1, m2])
+    return Aeff(fs, M, eta, Dl=Dl)
 
 # get phase of signal in frequency-domain
-def get_phase(fs, comp_masses):
-    M, eta = mass_transform(comp_masses)
+def get_phase(fs, params):
+    m1, m2, t0, phi0, Dl = params
+    M, eta = mass_transform([m1, m2])
     return Psieff(fs, M, eta, t0=t0, phi0=phi0)
 
 # get derivative of phase with respect to frequency
-def get_phase_deriv(fs, comp_masses):
-    M, eta = mass_transform(comp_masses)
+def get_phase_deriv(fs, params):
+    m1, m2, t0, phi0, Dl = params
+    M, eta = mass_transform([m1, m2])
     return dPsieff_df(fs, M, eta, t0=t0)
 
 
 # get frequency-domain waveform
-def get_waveform_freq(fs, comp_masses):
-    return get_amp(fs, comp_masses) * np.exp(1.j * get_phase(fs, comp_masses))
+def get_waveform_freq(fs, params):
+    return get_amp(fs, params) * np.exp(1.j * get_phase(fs, params))
 
 
 # plot waveform at measured parameters
